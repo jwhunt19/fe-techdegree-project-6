f0ee081ed041e3845fe07f16994f38f7e766d63c
William <jwhunt19@gmail.com>
2020-02-20 19:26:36 -0500

    Add button disable

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const phrase = document.getElementById('phrase');
 const startButton = document.getElementById('overlay');
 let missed = 0;
 let phrases = [
-    'A cat may look at a king',
-    'Strike while the iron is hot',
-    'Fake it til you make it',
-    'Zeal without knowledge is fire without light',
-    'Marry in haste repent at leisure'
+    'a cat may look at a king',
+    'atrike while the iron is hot',
+    'aake it til you make it',
+    'zeal without knowledge is fire without light',
+    'marry in haste repent at leisure'
 ];
 // Take array of strings, select random index and split into an array of characters
 function getRandomPhraseAsArray(array){
@@ -31,23 +31,33 @@ function addPhraseToDisplay(array){
 // Check if parameter matches list item with 'letter' class
 function checkLetter(buttonPressed){
     const letters = document.getElementsByClassName('letter');
-    for(i = 0; i < array.length; i++){
+    for(i = 0; i < letters.length; i++){
         if(letters[i].textContent === buttonPressed.textContent){
             const matchingLetter = letters[i];
             matchingLetter.classList.add('show');
-            return matchingLetter;
-        } else {
-            return null;
         }
+
     }
 }
-const phraseArray = getRandomPhraseAsArray(phrases);
-addPhraseToDisplay(phraseArray);
+
 // Hide the start overlay
 startButton.addEventListener('click', () => {
     if (event.target.textContent === 'Start Game'){
     startButton.style.visibility = "hidden";
-    console.log('test');
     }
 });
 
+// add chosen class, disable clicked buttons
+qwerty.addEventListener('click', () => {
+    let button = event.target;
+    if(button.tagName === 'BUTTON'){
+        button.classList.add('chosen');
+    }
+    if(button.className === 'chosen'){
+        button.disabled = true;
+    }
+    console.log(checkLetter(button));
+});
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
