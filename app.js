dae4976b907416871dd51bd695a8b54915ed52ff
William <jwhunt19@gmail.com>
2020-02-26 10:02:39 -0500

    Add letterFound const, fixed checkletter

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const startButton = document.getElementById('overlay');
 let missed = 0;
 let phrases = [
     'a cat may look at a king',
-    'atrike while the iron is hot',
-    'aake it til you make it',
+    'strike while the iron is hot',
+    'fake it til you make it',
     'zeal without knowledge is fire without light',
     'marry in haste repent at leisure'
 ];
+
 // Take array of strings, select random index and split into an array of characters
 function getRandomPhraseAsArray(array){
     let randomArrayIndex = array[Math.floor(Math.random() * array.length)];
     let splitArray = [...randomArrayIndex];
     return splitArray;
 }
+
 // Take phrase characters array, add to list item with 'letter' class unless its a space
 function addPhraseToDisplay(array){
     for(i = 0; i < array.length; i++){
@@ -28,16 +30,18 @@ function addPhraseToDisplay(array){
         ul.appendChild(node);
     }
 }
+
 // Check if parameter matches list item with 'letter' class
 function checkLetter(buttonPressed){
     const letters = document.getElementsByClassName('letter');
+    let matchingLetter = null;
     for(i = 0; i < letters.length; i++){
         if(letters[i].textContent === buttonPressed.textContent){
-            const matchingLetter = letters[i];
+            matchingLetter = letters[i];
             matchingLetter.classList.add('show');
         }
-
     }
+    return matchingLetter;
 }
 
 // Hide the start overlay
@@ -47,7 +51,7 @@ startButton.addEventListener('click', () => {
     }
 });
 
-// add chosen class, disable clicked buttons
+// add chosen class, disable clicked button, if null remove life 
 qwerty.addEventListener('click', () => {
     let button = event.target;
     if(button.tagName === 'BUTTON'){
@@ -56,7 +60,17 @@ qwerty.addEventListener('click', () => {
     if(button.className === 'chosen'){
         button.disabled = true;
     }
-    console.log(checkLetter(button));
+    const letterFound = checkLetter(button);
+    if(letterFound === null){
+        missed += 1;
+        const tries = document.getElementsByClassName('tries');
+        for(i = 4; i >= 0; i--){
+            if(tries[i].children[0].src.includes('images/liveHeart.png') === true){
+                tries[i].children[0].src = 'images/lostHeart.png';
+                break;
+            }
+        }
+    }
 });
 
 const phraseArray = getRandomPhraseAsArray(phrases);
